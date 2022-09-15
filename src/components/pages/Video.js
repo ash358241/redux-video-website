df272bdd0ae76fe364f61629e4e81c3d6daeffd1
Ashraful Islam <ashrafunit7@gmail.com>
2022-09-16 01:38:26 +0600

    single video is showing dynamically

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,15 +1,25 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../fatures/api/apiSlice";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
 
 export default function Video() {
+    let { videoId } = useParams()
+    const { data: video } = useGetVideoQuery(videoId)
+
     return (
         <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
             <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
                 <div className="grid grid-cols-3 gap-2 lg:gap-8">
                     <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
+                        {
+                            video?.id &&
+                            <>
+                                <Player title={video.title} link={video.link} />
+                                <Description title={video.title} date={video.date} description={video.description} />
+                            </>
+                        }
                     </div>
 
                     <RelatedVideos />
